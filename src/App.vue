23ce3664dcbbc4ec22cf0297f89ee0e921cfed74
TheDusty117 <gabrieledipassio@gmail.com>
2023-03-07 14:40:35 +0100

    FEAT proj. created, axios, sass bootstrp installed

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import 'bootstrap/dist/css/bootstrap.min.css';
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
