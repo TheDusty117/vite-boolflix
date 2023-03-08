fd4b31e770d2fcb0838605370fb9de89425e3235
TheDusty117 <gabrieledipassio@gmail.com>
2023-03-08 18:02:36 +0100

    FEAT star added, now need to work on logic

--- src/components/CardMain.vue
+++ src/components/CardMain.vue
@@ -57,7 +57,13 @@
     <!-- <p>{{ item.original_language }}</p> -->
     <!-- <p>{{ item.vote_average }}</p> -->
     <p>{{ convertVote(item.vote_average) }}</p>
-    <font-awesome-icon icon="fa-solid fa-star" />
+
+    <font-awesome-icon class="fullstar" icon="fa-solid fa-star" />
+    <font-awesome-icon class="emptystar" icon="fa-solid fa-star" />
+    <font-awesome-icon class="emptystar" icon="fa-solid fa-star" />
+    <font-awesome-icon class="emptystar" icon="fa-solid fa-star" />
+    <font-awesome-icon class="emptystar" icon="fa-solid fa-star" />
+
   </li>
 
 </template>
@@ -74,6 +80,14 @@ li{
   }
   h3, h4{
     font-size: 18px;
+  }
+
+  .emptystar{
+    color: grey;
+  }
+
+  .fullstar{
+    color: yellow
   }
 }
 
